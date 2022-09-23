57141825cf390735956bcd3539951501bb2a5fe8
Dillon Thompson <dillybar@pop-os.localdomain>
2022-09-22 23:10:46 -0500

    setup

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react"
+
+const Footer: FC = () => {
+  return (
+    <div>
+      <footer>
+        <h1>Hello world</h1>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react"
+
+const Header: FC = () => {
+  return (
+    <div>
+      <header>
+        <h1>Hello world</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Header
